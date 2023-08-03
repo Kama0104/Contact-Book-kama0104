13181569a9bc6a71c9a853ab35b412438e3da8e3
sam kamali <kama0104@algonquinlive.com>
2023-08-03 17:42:42 -0400

    updates

--- contact-book-project/src/components/ContactList.vue
+++ contact-book-project/src/components/ContactList.vue
@@ -1,12 +1,29 @@
 <template>
-    <!-- Display the list of contacts here -->
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      contacts: Array, // Array of contacts
+  <div>
+    <h2>Contacts</h2>
+    <ul>
+      <li v-for="contact in sortedContacts" :key="contact.id" @click="showContactDetails(contact)">
+        {{ contact.firstName }} {{ contact.lastName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    contacts: Array, // array of contacts
+  },
+  computed: {
+    sortedContacts() {
+      // Sorting contacts by last name
+      return this.contacts.sort((a, b) => a.lastName.localeCompare(b.lastName));
     },
-  };
-  </script>
-  
+  },
+  methods: {
+    showContactDetails(contact) {
+      this.$emit('show-details', contact.id);
+    },
+  },
+};
+</script>
